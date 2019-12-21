59ac38eafc75cf081b9de8d0695a297f519855c4
Roshan Lakmal <roshanlakmal@Roshans-MacBook-Air.local>
2019-12-21 22:03:52 +1100

    added oncreateServer method

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-
+ 
 @Component({
-  selector: 'app-servers',
-  // selector: '[app-servers]',
-  // selector: '.app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+ selector: 'app-servers',
+ templateUrl: './servers.component.html',
+ styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  constructor() { 
-    setTimeout(() => {
-      this.allowNewServer = true;
-    },2000);
-  }
-
-  ngOnInit() {
-  }
-
+ allowNewServer = false;
+ serverCreationStatus = 'No server was created!';
+ 
+ constructor() {
+   setTimeout(() => {
+     this.allowNewServer = true;
+   },2000);
+ }
+ 
+ ngOnInit() {
+ }
+ 
+ //Use on to say that this will be triggered by this template
+ onCreateServer() {
+   this.serverCreationStatus = 'Server was created!';
+ }
 }
